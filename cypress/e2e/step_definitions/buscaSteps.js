1d123013b441e95facc1026de031f24f012a9c3e
Ezequias <zekasantana.ccb_eu@hotmail.com>
2026-07-02 11:42:29 -0300

    fix: adiciona step de busca e fixture usuarios

--- cypress/e2e/step_definitions/buscaSteps.js
+++ cypress/e2e/step_definitions/buscaSteps.js
@@ -1,8 +1,6 @@
 import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import ProdutosPage from "../../pages/ProdutosPage";
 
-const produtosPage = ProdutosPage;
-
 When("pesquiso por um produto", () => {
   produtosPage.buscarProduto("Blue Top");
 });
