a227b8129f5e691232f72619e220396c4f2c55ff
Ezequias <zekasantana.ccb_eu@hotmail.com>
2026-07-02 11:29:38 -0300

    fix: adiciona validacao de busca

--- cypress/e2e/step_definitions/buscaSteps.js
+++ cypress/e2e/step_definitions/buscaSteps.js
@@ -1,8 +1,12 @@
-import { When } from "@badeball/cypress-cucumber-preprocessor";
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import ProdutosPage from "../../pages/ProdutosPage";
 
 const produtosPage = ProdutosPage;
 
 When("pesquiso por um produto", () => {
   produtosPage.buscarProduto("Blue Top");
+});
+
+Then("o produto deve ser exibido", () => {
+  cy.contains("Blue Top").should("be.visible");
 });
