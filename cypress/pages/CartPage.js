2d874a478805af871b9988ce3f147c2b369c3279
Ezequias <zekasantana.ccb_eu@hotmail.com>
2026-07-03 11:20:00 -0300

    fix: corrige acesso ao checkout pelo modal do carrinho

--- cypress/pages/CartPage.js
+++ cypress/pages/CartPage.js
@@ -1,6 +1,7 @@
 class CartPage {
   acessarCarrinho() {
-    cy.get('a[href="/view_cart"]').first().click();
+    cy.get("#cartModal").should("be.visible");
+    cy.contains("u", "View Cart").click();
   }
 
   validarProduto(produto) {
