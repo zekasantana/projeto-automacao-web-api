afbc4820383211f369a39ae096eb24487f6f82a4
Ezequias <zekasantana.ccb_eu@hotmail.com>
2026-07-07 14:01:04 -0300

    test: adiciona cenário negativo de produto inexistente

--- cypress/pages/ProdutosPage.js
+++ cypress/pages/ProdutosPage.js
@@ -18,6 +18,18 @@ class ProdutosPage {
     cy.get("#cartModal").should("be.visible");
     cy.contains("View Cart").click();
   }
+
+  buscarProdutoInexistente() {
+    cy.visit("/products");
+    cy.get("#search_product").type("ProdutoInexistente123");
+    cy.get("#submit_search").click();
+  }
+
+  validarNenhumProdutoEncontrado() {
+    cy.get(".features_items .product-image-wrapper")
+      .should("have.lenght",0);
+  }
+
 }
 
 export default new ProdutosPage();
