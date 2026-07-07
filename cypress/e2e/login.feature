50d2bf263a13d68aae2a83e66bafc835431fe83a
Ezequias <zekasantana.ccb_eu@hotmail.com>
2026-07-07 13:29:16 -0300

    test: adiciona cenário negativo de login inválido

--- cypress/e2e/login.feature
+++ cypress/e2e/login.feature
@@ -4,3 +4,8 @@ Feature: Login
     Given que acesso a tela de login
     When informo um e-mail e senha válidos
     Then devo visualizar a página principal
+
+  Scenario: Login com credenciais inválidas
+  Given que acesso a tela de login
+  When informo um e-mail e senha inválidos
+  Then devo visualizar a mensagem de erro de login
--- cypress/e2e/step_definitions/loginSteps.js
+++ cypress/e2e/step_definitions/loginSteps.js
@@ -16,3 +16,15 @@ Then("devo visualizar a página principal", () => {
   cy.url().should("include", "/");
   cy.get('a[href="/logout"]').should("be.visible");
 });
+
+When("informo um e-mail e senha inválidos", () => {
+
+cy.get('[data-qa="login-email"]').type("teste@invalido.com");
+    cy.get('[data-qa="login-password"]').type("654321");
+    cy.get('[data-qa="login-button"]').click();
+  });
+
+
+Then("devo visualizar a mensagem de erro de login", () => {
+  cy.contains("Your email or password is incorrect!").should("be.visible");
+});
